f8eca396b1cabd12a30bfca117715fa1df748cf9
Anni-Ly <84103109+Anni-Ly@users.noreply.github.com>
2021-06-04 22:48:52 +0200

    Update DataValidator.java

--- DataValidator.java
+++ DataValidator.java
@@ -10,23 +10,18 @@ import java.util.regex.Pattern;
 
 public class DataValidator {
 
-    // Class used to check if input data is valid
-
     public static boolean isValidCPR(String cpr) {
         Boolean result = false;
-        // Check the lenght
         if (cpr.length() == 11) {
             result = true;
         } else {
             return false;
         }
 
-        // Check the format (two numbers)
-        // first split the cpr number
         String birthday = cpr.substring(0,6);
         Character hyphen = cpr.charAt(6);
         String lastNumber = cpr.substring(7,11);
-        try { // test if the first part is a number }
+        try { 
             int d = Integer.parseInt(birthday);
         } catch (NumberFormatException nfe) {
             System.out.println("First part is not a number");
@@ -35,15 +30,13 @@ public class DataValidator {
         if (! hyphen.equals('-')) {
             System.out.println("Missing hyphen");
             return false; };
-        try { // test if the last part is a number }
+        try { 
             int running = Integer.parseInt(lastNumber);
         } catch (NumberFormatException nfe) {
             System.out.println("Last part is not a number");
             return false;
         }
 
-        // a much easier approach with regex
-        // and the extract the first and last part..
         if (cpr.matches("^[0-9]{6}-[0-9]{4}$")) {
             System.out.println("matches regex");
             result = true;
@@ -52,7 +45,6 @@ public class DataValidator {
             result = false;
         }
 
-        // now test the date using LocalDate
         try {
             DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("ddMMuu");
             LocalDate b = LocalDate.parse(birthday, dateFormatter);
@@ -66,7 +58,6 @@ public class DataValidator {
         return result;
     }
     public static boolean isValidName(String name) {
-        // Sorry Marie-Louise.... '-' not allowed in this stupid validator
 
         if (name.length() > 1 &&
                 name.chars().allMatch(Character::isAlphabetic)) {
@@ -77,7 +68,6 @@ public class DataValidator {
     }
 
     public static boolean isValidEmail(String email) {
-        // https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s03.html
         final String EMAIL_PATTERN =
                 "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@"
                         + "[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";
@@ -116,9 +106,6 @@ public class DataValidator {
     }
 
 }
-
-//DataSenor skal der ikke laves - men husk at skrive i det opgaven
-
 
 
 

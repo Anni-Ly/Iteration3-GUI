ac905411e2e2f465c715685793ab8796d70772f5
Anni-Ly <84103109+Anni-Ly@users.noreply.github.com>
2021-06-04 22:38:03 +0200

    Update ReportController1.java

--- ReportController1.java
+++ ReportController1.java
@@ -29,7 +29,6 @@ public class ReportController1 {
     private ReportRegister reportRegister = new ReportRegister();
 
     private void clearTextFields(GridPane grid) {
-        // Simple method to clear the text fields in a grid
         for (Node node : grid.getChildren()) {
             if (node instanceof TextField) {
                 // clear
@@ -40,9 +39,6 @@ public class ReportController1 {
     }
 
     private void resetTextFields(GridPane grid) {
-        // Simple method to reset the style on text fields in a grid which have a style set
-        // and don't touch the other fields
-        // We use it to clear the fields we turned pink
         for (Node node : grid.getChildren()) {
             if (node instanceof TextField) {
                 System.out.println(node.getStyle());
@@ -56,9 +52,6 @@ public class ReportController1 {
     }
 
     public static String capitalize(String str) {
-        // Method to make names appear with capital first letter
-        // also turn uppercase letters not in first position into lowercase
-        // sorry McDonald!
         if (str == null || str.isEmpty()) {
             return str;
         }
@@ -76,25 +69,21 @@ public class ReportController1 {
 
         System.out.println(r1);
 
-        // Set som more attributes for p2;
         r2.setDato("2020-05-31");
         r2.setPostnr("2610");
         r2.setCoronaVariant("indisk");
 
         System.out.println(r2);
 
-        //PatientRegister patientRegister = new PatientRegister();
         try {
             reportRegister.addReport(r1);
         } catch (Exception e) {
             e.printStackTrace();
-            //System.out.println(e);
         }
         try {
             reportRegister.addReport(r2);
         } catch (Exception e) {
             e.printStackTrace();
-            //System.out.println(e);
         }
 
         reportRegister.printReports();
@@ -122,12 +111,9 @@ public class ReportController1 {
         String CoronaVariantInput = capitalize(CoronaVariantText.getText());
         boolean validInput = true;
 
-        // validate the user input
 
 
         if (!ReportValidator.isValidDato(DatoInput)) {
-            // we need a CPR number in order to register a user
-            // highlight the CPR input field if content is invalid
             DatoText.setStyle("-fx-background-color: pink");
             validInput = false;
         }
@@ -140,10 +126,6 @@ public class ReportController1 {
             CoronaVariantText.setStyle("-fx-background-color: pink");
             validInput = false;
         }
-
-
-        // Now register the patient in the patient register
-        // alert the user if the patient is already there
 
 
         if (validInput) {
@@ -178,7 +160,6 @@ public class ReportController1 {
             alert.showAndWait();
             resetTextFields(grid);
         }
-            // list the patients
         ReportListTextArea.setText(reportRegister.listReports());
 
     }
